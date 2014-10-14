ae4e65fc173fcfabdd6d0275e010bc321f8cb421
Elvis Nuñez <elvisnunez@me.com>
2014-10-14 20:51:34 +0200

    Remove unused elements

--- Source/Formatters/HYPFormatter.m
+++ Source/Formatters/HYPFormatter.m
@@ -6,19 +6,8 @@
 //
 
 #import "HYPFormatter.h"
-#import "NSString+ZENInflections.h"
-
-static NSString * const HYPFormatterClass = @"HYP%@Formatter";
-static NSString * const HYPFormatterSelector = @"formatString:reverse:";
 
 @implementation HYPFormatter
-
-+ (Class)formatterClass:(NSString *)string
-{
-    NSString *classString = [NSString stringWithFormat:HYPFormatterClass, [string zen_upperCamelCase]];
-
-    return NSClassFromString(classString);
-}
 
 - (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse
 {
