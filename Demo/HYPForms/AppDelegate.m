7e88edfc71120db9ce20850d2a3e906f0986ae1d
Christoffer Winterkvist <chris@zenangst.com>
2014-10-17 10:14:47 +0200

    Add dummy data

--- Demo/HYPForms/AppDelegate.m
+++ Demo/HYPForms/AppDelegate.m
@@ -36,7 +36,11 @@
                                   @"start_date" : @"2014-10-31 23:00:00 +00:00",
                                   @"worker_id" : @"120000",
                                   @"employment_type" : @"fast",
-                                  @"remuneration" : @"no_wage"
+                                  @"remuneration" : @"no_wage",
+                                  @"employment_percent" : @"100",
+                                  @"fixed_pay_level" : @"17900",
+                                  @"fixed_pay_premium_percent" : @"2",
+                                  @"fixed_pay_premium_currency" : @"150"
                                   };
 
     HYPSampleCollectionViewController *controllers = [[HYPSampleCollectionViewController alloc] initWithDictionary:dictionary];
