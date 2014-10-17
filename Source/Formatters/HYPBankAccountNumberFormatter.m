0c0454b1423cb0a194a0ee94c6cf4023e9f06eda
Christoffer Winterkvist <chris@zenangst.com>
2014-10-17 11:06:54 +0200

    Return nil if string is not present

--- Source/Formatters/HYPBankAccountNumberFormatter.m
+++ Source/Formatters/HYPBankAccountNumberFormatter.m
@@ -12,6 +12,7 @@
 - (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse
 {
     string = [super formatString:string reverse:reverse];
+    if (!string) return nil;
 
     if (reverse) {
         return [string stringByReplacingOccurrencesOfString:@"." withString:@""];
