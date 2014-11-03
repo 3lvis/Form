0588759941e5a5f6de2010a73713c1ff04d2a9e3
Christoffer Winterkvist <chris@zenangst.com>
2014-11-03 21:07:27 +0100

    Refactor SSN formatter

--- Source/Formatters/HYPSocialSecurityNumberFormatter.m
+++ Source/Formatters/HYPSocialSecurityNumberFormatter.m
@@ -14,17 +14,25 @@
     string = [super formatString:string reverse:reverse];
     if (!string) return nil;
 
+    NSString *rawString = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
     if (reverse) {
-        return [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+        return rawString;
     }
 
-    NSMutableString *mutableString = [[NSMutableString alloc] initWithString:string];
+    NSMutableString *mutableString = [NSMutableString new];
+    NSUInteger idx = 0;
+    NSString *characterString;
 
-    if ([mutableString length] > 6 && ![[mutableString substringWithRange:NSMakeRange(6,1)] isEqualToString:@" "]) {
-        [mutableString insertString:@" " atIndex:6];
+    while (idx < rawString.length) {
+        characterString = [NSString stringWithFormat:@"%c", [rawString characterAtIndex:idx]];
+        [mutableString appendString:characterString];
+
+        if (idx == 5) [mutableString appendString:@" "];
+
+        ++idx;
     }
 
-    return [mutableString copy];
+    return mutableString;
 }
 
 @end
