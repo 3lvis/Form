90d89d0b7172c806de327753507a848c755210a2
Elvis Nuñez <elvisnunez@me.com>
2014-11-05 13:31:32 +0100

    Refactor form time controller

--- Source/Others/HYPFormTimeViewController.m
+++ Source/Others/HYPFormTimeViewController.m
@@ -13,14 +13,14 @@
 
 static const CGSize HYPDatePopoverSize = { 320.0f, 216.0f };
 
-static CGFloat const HYPActionButtonX = 10.0f;
-static CGFloat const HYPActionButtonHeight = 45.0f;
+static CGFloat const HYPDoneButtonX = 10.0f;
+static CGFloat const HYPDoneButtonHeight = 45.0f;
 
 @interface HYPFormTimeViewController ()
 
 @property (nonatomic, strong) UIDatePicker *datePicker;
 
-@property (nonatomic, strong) UIButton *actionButton;
+@property (nonatomic, strong) UIButton *doneButton;
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UITextView *messageTextView;
 
@@ -56,27 +56,25 @@ static CGFloat const HYPActionButtonHeight = 45.0f;
     return _datePicker;
 }
 
-- (UIButton *)actionButton
+- (UIButton *)doneButton
 {
-    if (_actionButton) return _actionButton;
+    if (_doneButton) return _doneButton;
 
-    CGFloat height = HYPActionButtonHeight;
-    CGFloat offset = HYPActionButtonX;
+    CGFloat height = HYPDoneButtonHeight;
+    CGFloat offset = HYPDoneButtonX;
     CGFloat y = 0.0f;
 
-    if (self.date) {
-        y = CGRectGetMaxY(self.datePicker.frame);
-    }
+    if (self.date) y = CGRectGetMaxY(self.datePicker.frame);
 
-    _actionButton = [[UIButton alloc] initWithFrame:CGRectMake(offset, y,
-                                                               HYPDatePopoverSize.width - (offset * 2.0f), height)];
-    [_actionButton setTitle:@"Done" forState:UIControlStateNormal];
-    _actionButton.backgroundColor = [UIColor REMACallToAction];
-    [_actionButton addTarget:self action:@selector(actionButtonPressed) forControlEvents:UIControlEventTouchUpInside];
-    _actionButton.layer.cornerRadius = 5.0f;
-    _actionButton.titleLabel.font = [UIFont REMALargeSizeBold];
+    _doneButton = [[UIButton alloc] initWithFrame:CGRectMake(offset, y,
+                                                             HYPDatePopoverSize.width - (offset * 2.0f), height)];
+    [_doneButton setTitle:NSLocalizedString(@"Done", nil) forState:UIControlStateNormal];
+    _doneButton.backgroundColor = [UIColor REMACallToAction];
+    [_doneButton addTarget:self action:@selector(doneButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    _doneButton.layer.cornerRadius = 5.0f;
+    _doneButton.titleLabel.font = [UIFont REMALargeSizeBold];
 
-    return _actionButton;
+    return _doneButton;
 }
 
 #pragma mark - Setters
@@ -103,11 +101,10 @@ static CGFloat const HYPActionButtonHeight = 45.0f;
 
     if (self.date) {
         [self.view addSubview:self.datePicker];
-
         [self.datePicker setDate:self.date];
     }
 
-    [self.view addSubview:self.actionButton];
+    [self.view addSubview:self.doneButton];
 }
 
 #pragma mark - Actions
@@ -117,7 +114,7 @@ static CGFloat const HYPActionButtonHeight = 45.0f;
     self.date = datePicker.date;
 }
 
-- (void)actionButtonPressed
+- (void)doneButtonPressed
 {
     if ([self.delegate respondsToSelector:@selector(timeController:didChangedDate:)]) {
         [self.delegate timeController:self didChangedDate:self.datePicker.date];
