21c6107ad78d713da3cc7f9ca9373c5510c8b2c3
Christoffer Winterkvist <chris@zenangst.com>
2014-11-06 21:01:20 +0100

    Slaughter - (void)reloadWithDictionary:(NSDictionary *)dictionary

--- Source/HYPFormsCollectionViewDataSource.m
+++ Source/HYPFormsCollectionViewDataSource.m
@@ -330,12 +330,6 @@
     [self resetForms];
 }
 
-- (void)reloadWithDictionary:(NSDictionary *)dictionary
-{
-    [self reloadWithDictionary:dictionary completion:nil];
-}
-
-
 - (void)reloadWithDictionary:(NSDictionary *)dictionary completion:(void (^)())completion
 {
     [self.valuesDictionary setValuesForKeysWithDictionary:dictionary];
