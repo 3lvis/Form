128b6bfce4b74f759bed11ba12a09b5c456a6c18
Christoffer Winterkvist <chris@zenangst.com>
2014-11-13 19:39:11 +0100

    Remove header

--- Source/Input Validators/HYPNameInputValidator.m
+++ Source/Input Validators/HYPNameInputValidator.m
@@ -1,10 +1,3 @@
-//
-//  HYPNameInputValidator.m
-//
-//  Created by Christoffer Winterkvist on 10/1/14.
-//  Copyright (c) 2014 Hyper. All rights reserved.
-//
-
 #import "HYPNameInputValidator.h"
 
 @implementation HYPNameInputValidator
