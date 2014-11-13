5ee3c8a25b676caed079e23b795e638492b48651
Elvis Nuñez <elvisnunez@me.com>
2014-11-13 20:31:53 +0100

    Remove headers

--- Demo/HYPForms/AppDelegate.h
+++ Demo/HYPForms/AppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.h
-//
-//  Created by Elvis Nunez on 03/10/14.
-//  Copyright (c) 2014 Hyper. All rights reserved.
-//
-
 @import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- Demo/HYPForms/HYPSampleCollectionViewController.m
+++ Demo/HYPForms/HYPSampleCollectionViewController.m
@@ -1,11 +1,3 @@
-//
-//  HYPFormsCollectionViewController.m
-
-//
-//  Created by Elvis Nunez on 03/10/14.
-//  Copyright (c) 2014 Hyper. All rights reserved.
-//
-
 #import "HYPSampleCollectionViewController.h"
 
 #import "HYPFormsCollectionViewDataSource.h"
