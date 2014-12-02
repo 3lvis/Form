5f8d470a1ea5b0832fdbb0d1f9e73b736421b8f3
Christoffer Winterkvist <chris@zenangst.com>
2014-12-02 21:35:52 +0100

    Move invalidFormFields method to forms manager

--- Demo/HYPFormsTests/HYPFormsCollectionViewDataSourceTests.m
+++ Demo/HYPFormsTests/HYPFormsCollectionViewDataSourceTests.m
@@ -93,7 +93,7 @@
 
 - (void)testFieldValidation
 {
-    NSArray *fields = [self.dataSource invalidFormFields];
+    NSArray *fields = [self.formsManager invalidFormFields];
 
     XCTAssertNotNil(fields);
 }
