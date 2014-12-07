9460c3325c33ed6e7d9b4a370489056514d76dbe
Elvis Nuñez <elvisnunez@me.com>
2014-12-07 11:56:31 +0100

    Connect fields with their parent sections

--- Demo/HYPFormsTests/HYPFormSection+Tests.m
+++ Demo/HYPFormsTests/HYPFormSection+Tests.m
@@ -9,9 +9,19 @@
     HYPFormSection *section = [HYPFormSection new];
     section.sectionID = @"personal_details-0";
     section.position = @0;
-    section.fields = [@[[HYPFormField firstNameField],
-                        [HYPFormField lastNameField],
-                        [HYPFormField displayNameField]] mutableCopy];
+
+    HYPFormField *firstNameField = [HYPFormField firstNameField];
+    firstNameField.section = section;
+
+    HYPFormField *lastNameField = [HYPFormField lastNameField];
+    lastNameField.section = section;
+
+    HYPFormField *displayNameField = [HYPFormField displayNameField];
+    displayNameField.section = section;
+
+    section.fields = [@[firstNameField,
+                        lastNameField,
+                        displayNameField] mutableCopy];
 
     return section;
 }
@@ -21,8 +31,19 @@
     HYPFormSection *section = [HYPFormSection new];
     section.sectionID = @"personal_details-1";
     section.position = @1;
-    section.fields = [@[[HYPFormField emailField],
-                        [HYPFormField usernameField]] mutableCopy];
+
+    HYPFormField *addressField = [HYPFormField addressField];
+    addressField.section = section;
+
+    HYPFormField *emailField = [HYPFormField emailField];
+    emailField.section = section;
+
+    HYPFormField *usernameField = [HYPFormField usernameField];
+    usernameField.section = section;
+
+    section.fields = [@[addressField,
+                        emailField,
+                        usernameField] mutableCopy];
 
     return section;
 }
@@ -32,8 +53,15 @@
     HYPFormSection *section = [HYPFormSection new];
     section.sectionID = @"employment-0";
     section.position = @0;
-    section.fields = [@[[HYPFormField workHoursField],
-                        [HYPFormField workerTypeField]] mutableCopy];
+
+    HYPFormField *workHoursField = [HYPFormField workHoursField];
+    workHoursField.section = section;
+
+    HYPFormField *workerTypeField = [HYPFormField workerTypeField];
+    workerTypeField.section = section;
+
+    section.fields = [@[workHoursField,
+                        workerTypeField] mutableCopy];
 
     return section;
 }
@@ -43,9 +71,19 @@
     HYPFormSection *section = [HYPFormSection new];
     section.sectionID = @"employment-1";
     section.position = @1;
-    section.fields = [@[[HYPFormField startDateField],
-                        [HYPFormField endDateField],
-                        [HYPFormField contractTypeField]] mutableCopy];
+
+    HYPFormField *startDateField = [HYPFormField startDateField];
+    startDateField.section = section;
+
+    HYPFormField *endDateField = [HYPFormField endDateField];
+    endDateField.section = section;
+
+    HYPFormField *contractTypeField = [HYPFormField contractTypeField];
+    contractTypeField.section = section;
+
+    section.fields = [@[startDateField,
+                        endDateField,
+                        contractTypeField] mutableCopy];
 
     return section;
 }
@@ -55,10 +93,23 @@
     HYPFormSection *section = [HYPFormSection new];
     section.sectionID = @"employment-2";
     section.position = @2;
-    section.fields = [@[[HYPFormField baseSalaryTypeField],
-                        [HYPFormField bonusEnabledField],
-                        [HYPFormField bonusField],
-                        [HYPFormField totalField]] mutableCopy];
+
+    HYPFormField *baseSalaryTypeField = [HYPFormField baseSalaryTypeField];
+    baseSalaryTypeField.section = section;
+
+    HYPFormField *bonusEnabledField = [HYPFormField bonusEnabledField];
+    bonusEnabledField.section = section;
+
+    HYPFormField *bonusField = [HYPFormField bonusField];
+    bonusField.section = section;
+
+    HYPFormField *totalField = [HYPFormField totalField];
+    totalField.section = section;
+
+    section.fields = [@[baseSalaryTypeField,
+                        bonusEnabledField,
+                        bonusField,
+                        totalField] mutableCopy];
 
     return section;
 }
