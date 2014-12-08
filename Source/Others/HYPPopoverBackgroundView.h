300d8d92eeecaad9521d69353c0f76152fcecaa8
Christoffer Winterkvist <chris@zenangst.com>
2014-12-08 22:25:20 +0100

    Implement custom HYPPopoverBackgroundView

--- Source/Others/HYPPopoverBackgroundView.h
+++ Source/Others/HYPPopoverBackgroundView.h
@@ -2,4 +2,16 @@
 
 @interface HYPPopoverBackgroundView : UIPopoverBackgroundView
 
+@property (nonatomic) CGFloat arrowOffset;
+@property (nonatomic) UIPopoverArrowDirection arrowDirection;
+
++ (void)setContentInset:(CGFloat)contentInset;
++ (void)setTintColor:(UIColor *)tintColor;
++ (void)setShadowEnabled:(BOOL)shadowEnabled;
++ (void)setArrowBase:(CGFloat)arrowBase;
++ (void)setArrowHeight:(CGFloat)arrowHeight;
++ (void)setBackgroundImageCornerRadius:(CGFloat)cornerRadius;
++ (void)setBackgroundImage:(UIImage *)background top:(UIImage *)top right:(UIImage *)right bottom:(UIImage *)bottom left:(UIImage *)left;
++ (void)rebuildArrowImages;
+
 @end
