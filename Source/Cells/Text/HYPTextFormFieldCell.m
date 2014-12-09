bffd9f5b02aaf652810e1ca00a3f7e47e29b0886
Christoffer Winterkvist <chris@zenangst.com>
2014-12-09 09:17:52 +0100

    Let the popover live

--- Source/Cells/Text/HYPTextFormFieldCell.m
+++ Source/Cells/Text/HYPTextFormFieldCell.m
@@ -169,8 +169,6 @@
 - (void)textFormFieldDidBeginEditing:(HYPTextField *)textField
 {
     if (self.field.subtitle) {
-        if (self.popoverController) self.popoverController = nil;
-
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self.popoverController presentPopoverFromRect:[self popoverFrame] inView:self.textField permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
         });
