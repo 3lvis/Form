4b61bee90c2f6a494c88fc2583934a80d9dd0467
Christoffer Winterkvist <chris@zenangst.com>
2014-12-09 09:18:15 +0100

    Code style

--- Source/Cells/Text/HYPTextFormFieldCell.m
+++ Source/Cells/Text/HYPTextFormFieldCell.m
@@ -170,7 +170,9 @@
 {
     if (self.field.subtitle) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self.popoverController presentPopoverFromRect:[self popoverFrame] inView:self.textField permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+            [self.popoverController presentPopoverFromRect:[self popoverFrame]
+                                                    inView:self.textField permittedArrowDirections:UIPopoverArrowDirectionAny
+                                                  animated:YES];
         });
     }
 }
