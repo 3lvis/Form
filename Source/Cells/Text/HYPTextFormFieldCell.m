ebc08114a2e07ce8bc4022eadf469a43d9f23f38
Christoffer Winterkvist <chris@zenangst.com>
2014-12-09 09:18:26 +0100

    Even prettier

--- Source/Cells/Text/HYPTextFormFieldCell.m
+++ Source/Cells/Text/HYPTextFormFieldCell.m
@@ -171,7 +171,8 @@
     if (self.field.subtitle) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self.popoverController presentPopoverFromRect:[self popoverFrame]
-                                                    inView:self.textField permittedArrowDirections:UIPopoverArrowDirectionAny
+                                                    inView:self.textField
+                                  permittedArrowDirections:UIPopoverArrowDirectionAny
                                                   animated:YES];
         });
     }
