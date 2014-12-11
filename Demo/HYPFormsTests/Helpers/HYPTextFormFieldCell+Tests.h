a6e4febce9354aee7e7593558a529945cafb7cc0
Christoffer Winterkvist <chris@zenangst.com>
2014-12-11 19:40:34 +0100

    Moar fire

--- Demo/HYPFormsTests/Helpers/HYPTextFormFieldCell+Tests.h
+++ Demo/HYPFormsTests/Helpers/HYPTextFormFieldCell+Tests.h
@@ -1,11 +1,3 @@
-//
-//  HYPTextFormFieldCell+Tests.h
-//  HYPForms
-//
-//  Created by Christoffer Winterkvist on 12/11/14.
-//  Copyright (c) 2014 Hyper. All rights reserved.
-//
-
 #import "HYPTextFormFieldCell.h"
 
 @interface HYPTextFormFieldCell (Tests)
--- Demo/HYPFormsTests/Helpers/HYPTextFormFieldCell+Tests.m
+++ Demo/HYPFormsTests/Helpers/HYPTextFormFieldCell+Tests.m
@@ -1,11 +1,3 @@
-//
-//  HYPTextFormFieldCell+Tests.m
-//  HYPForms
-//
-//  Created by Christoffer Winterkvist on 12/11/14.
-//  Copyright (c) 2014 Hyper. All rights reserved.
-//
-
 #import "HYPTextFormFieldCell+Tests.h"
 
 @implementation HYPTextFormFieldCell (Tests)
