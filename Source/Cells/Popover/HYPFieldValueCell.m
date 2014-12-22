b9678e3806457d78832b6207dc4d399719d9e81b
Christoffer Winterkvist <chris@zenangst.com>
2014-12-22 10:54:32 +0100

    Add subtitles to field value cell

--- Source/Cells/Popover/HYPFieldValueCell.m
+++ Source/Cells/Popover/HYPFieldValueCell.m
@@ -11,13 +11,19 @@
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
     if (!self) return nil;
 
     self.textLabel.font = [UIFont HYPFormsMediumSize];
     self.textLabel.textColor = [UIColor HYPFormsDarkBlue];
     self.textLabel.highlightedTextColor = [UIColor whiteColor];
     self.textLabel.textAlignment = NSTextAlignmentLeft;
+
+    self.detailTextLabel.font = [UIFont HYPFormsSmallSize];
+    self.detailTextLabel.textColor = [UIColor HYPFormsDarkBlue];
+    self.detailTextLabel.highlightedTextColor = [UIColor whiteColor];
+    self.detailTextLabel.textAlignment = NSTextAlignmentLeft;
+
     self.selectionStyle = UITableViewCellSelectionStyleGray;
     self.backgroundColor = [UIColor whiteColor];
     self.separatorInset = UIEdgeInsetsZero;
@@ -37,6 +43,10 @@
     _fieldValue = fieldValue;
 
     self.textLabel.text = fieldValue.title;
+
+    if (fieldValue.subtitle) {
+        self.detailTextLabel.text = fieldValue.subtitle;
+    }
 }
 
 #pragma mark - Overwritables
