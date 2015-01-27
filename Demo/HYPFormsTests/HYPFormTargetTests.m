e530a837075da32641213755d5be2d9a69782a55
Elvis Nuñez <elvisnunez@me.com>
2015-01-28 00:31:42 +0100

    Test filteredTargets

--- Demo/HYPFormsTests/HYPFormTargetTests.m
+++ Demo/HYPFormsTests/HYPFormTargetTests.m
@@ -51,4 +51,44 @@
     XCTAssertTrue(target.actionType == HYPFormTargetActionEnable);
 }
 
+- (void)testFilteredTargets
+{
+    NSMutableArray *targets = [NSMutableArray new];
+
+    [targets addObject:[HYPFormTarget showFieldTargetWithID:@"first_name"]];
+    [targets addObject:[HYPFormTarget showFieldTargetWithID:@"first_name"]];
+    [targets addObject:[HYPFormTarget showFieldTargetWithID:@"first_name"]];
+
+    [targets addObject:[HYPFormTarget hideFieldTargetWithID:@"last_name"]];
+    [targets addObject:[HYPFormTarget hideFieldTargetWithID:@"last_name"]];
+    [targets addObject:[HYPFormTarget hideFieldTargetWithID:@"last_name"]];
+
+    [targets addObject:[HYPFormTarget enableFieldTargetWithID:@"start_date"]];
+    [targets addObject:[HYPFormTarget enableFieldTargetWithID:@"start_date"]];
+    [targets addObject:[HYPFormTarget enableFieldTargetWithID:@"start_date"]];
+
+    [targets addObject:[HYPFormTarget disableFieldTargetWithID:@"end_date"]];
+    [targets addObject:[HYPFormTarget disableFieldTargetWithID:@"end_date"]];
+    [targets addObject:[HYPFormTarget disableFieldTargetWithID:@"end_date"]];
+
+    [targets addObject:[HYPFormTarget disableFieldTargetWithID:@"end_date"]];
+    [targets addObject:[HYPFormTarget disableFieldTargetWithID:@"end_date"]];
+    [targets addObject:[HYPFormTarget disableFieldTargetWithID:@"end_date"]];
+
+    [targets addObject:[HYPFormTarget updateFieldTargetWithID:@"bonus_enabled"]];
+    [targets addObject:[HYPFormTarget updateFieldTargetWithID:@"bonus_enabled"]];
+    [targets addObject:[HYPFormTarget updateFieldTargetWithID:@"bonus_enabled"]];
+
+    [HYPFormTarget filteredTargets:targets
+                          filtered:^(NSArray *shownTargets, NSArray *hiddenTargets, NSArray *updatedTargets,
+                                     NSArray *enabledTargets, NSArray *disabledTargets) {
+                              XCTAssertEqual(shownTargets.count, 1);
+                              XCTAssertEqual(hiddenTargets.count, 1);
+                              XCTAssertEqual(updatedTargets.count, 1);
+                              XCTAssertEqual(enabledTargets.count, 1);
+                              XCTAssertEqual(disabledTargets.count, 1);
+                          }];
+
+}
+
 @end
