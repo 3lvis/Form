96bc2e45e67e7bde01d8612dc574b0606a089eec
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-02-24 23:33:21 +0100

    First commit

---
+++ Demos/Login-demo/Login-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Login-demo
+//
+//  Created by Ramon Gilabert Llop on 2/24/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demos/Login-demo/Login-demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Login-demo
+//
+//  Created by Ramon Gilabert Llop on 2/24/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
