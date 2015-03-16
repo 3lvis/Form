92e5fe5ccde425b45bf5680974bd353c275108c5
Elvis Nuñez <elvisnunez@me.com>
2015-03-17 00:57:49 +0100

    Move logic to UIViewController-HYPKeyboardToolbar

--- Demos/Basic-ObjC/Basic-ObjC/Controllers/HYPSampleCollectionViewController.m
+++ Demos/Basic-ObjC/Basic-ObjC/Controllers/HYPSampleCollectionViewController.m
@@ -11,6 +11,7 @@
 
 #import "NSObject+HYPTesting.h"
 #import "UIColor+Hex.h"
+#import "UIViewController+HYPKeyboardToolbar.h"
 
 @interface HYPSampleCollectionViewController () <HYPImagePickerDelegate>
 
@@ -28,8 +29,7 @@
 
 - (void)dealloc
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidHideNotification object:nil];
+    [self hyp_removeKeyboardToolbarObservers];
 }
 
 #pragma mark - Initialization
@@ -51,15 +51,7 @@
         [self.collectionView numberOfSections];
     }
 
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWillShow:)
-                                                 name:UIKeyboardWillShowNotification
-                                               object:nil];
-
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWillHide:)
-                                                 name:UIKeyboardWillHideNotification
-                                               object:nil];
+    [self hyp_addKeyboardToolbarObservers];
 
     return self;
 }
@@ -172,18 +164,6 @@
     [self.navigationController setToolbarHidden:NO animated:YES];
 }
 
-#pragma mark - Notifications
-
-- (void)keyboardWillShow:(NSNotification *)notification
-{
-    [self keyboardWillToggle:notification];
-}
-
-- (void)keyboardWillHide:(NSNotification *)notification
-{
-    [self keyboardWillToggle:notification];
-}
-
 #pragma mark - UICollectionViewDelegate
 
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
@@ -270,36 +250,6 @@
     }
 
     [self.dataSource processTargets:@[target]];
-}
-
-#pragma mark - Private methods
-
-- (void)keyboardWillToggle:(NSNotification *)notification
-{
-    CGRect screenRect = [[UIScreen mainScreen] bounds];
-    CGRect navigationControllerToolbarRect = self.navigationController.toolbar.frame;
-    CGFloat initialY = screenRect.size.height - navigationControllerToolbarRect.size.height;
-
-    NSDictionary *userInfo = [notification userInfo];
-    NSTimeInterval animationDuration;
-    UIViewAnimationCurve animationCurve;
-    CGRect keyboardFrame;
-
-    [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] getValue:&animationCurve];
-    [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] getValue:&animationDuration];
-    [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] getValue:&keyboardFrame];
-
-    CGFloat navigationControllerToolbarY = initialY - keyboardFrame.size.height;
-    if ([notification.name isEqualToString:UIKeyboardWillHideNotification]) {
-        navigationControllerToolbarY = initialY;
-    }
-
-    CGRect rect = CGRectMake(navigationControllerToolbarRect.origin.x,
-                             navigationControllerToolbarY,
-                             navigationControllerToolbarRect.size.width,
-                             navigationControllerToolbarRect.size.height);
-
-    [self.navigationController.toolbar setFrame:rect];
 }
 
 @end
