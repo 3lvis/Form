040ef15c39665aaa9c896abce407a6c9263d7ad8
Christoffer Winterkvist <chris@zenangst.com>
2015-03-18 10:43:48 +0100

    Remove overriding setter

--- Source/Models/FORMSection.m
+++ Source/Models/FORMSection.m
@@ -80,22 +80,6 @@
     }
 }
 
-- (void)setPosition:(NSNumber *)position
-{
-    _position = position;
-
-    for (FORMField *field in self.fields) {
-        BOOL isDynamic = ([field.fieldID hyp_containsString:@"["] &&
-                          [field.fieldID hyp_containsString:@"]"]);
-        if (isDynamic) {
-            NSDictionary *parsed = [field.fieldID hyp_parseRelationship];
-            NSInteger newPosition = [position integerValue] - 1;
-            NSString *newKey = [NSString stringWithFormat:@"%@[%@].%@", [parsed objectForKey:@"relationship"], @(newPosition), [parsed objectForKey:@"attribute"]];
-
-            field.fieldID = newKey;
-        }
-    }
-}
 
 #pragma mark - Public Methods
 
