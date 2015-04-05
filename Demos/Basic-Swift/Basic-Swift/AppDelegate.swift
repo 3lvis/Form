3a82fed9ec9514c681499c5b086718a1c198bfe0
Christoffer Winterkvist <chris@zenangst.com>
2015-04-05 17:40:35 +0200

    Refactor AppDelegate

--- Demos/Basic-Swift/Basic-Swift/AppDelegate.swift
+++ Demos/Basic-Swift/Basic-Swift/AppDelegate.swift
@@ -6,25 +6,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        FORMDefaultStyle.applyStyle()
 
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-
-        let initialValues: Dictionary<NSObject, AnyObject> = [
-            "address"    : "Burger Park 667",
-            "end_date"   : "2017-10-31 23:00:00 +00:00",
-            "first_name" : "Ola",
-            "last_name"  : "Nordman",
-            "start_date" : "2014-10-31 23:00:00 +00:00"]
-        var JSON: AnyObject? = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json")
-
-        if (JSON != nil) {
+        if let JSON: AnyObject? = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json") {
+            let initialValues: Dictionary<NSObject, AnyObject> = [
+                "address"    : "Burger Park 667",
+                "end_date"   : "2017-10-31 23:00:00 +00:00",
+                "first_name" : "Ola",
+                "last_name"  : "Nordman",
+                "start_date" : "2014-10-31 23:00:00 +00:00"]
             let sampleController = SampleCollectionViewController(initialValues: initialValues, JSON: JSON)
             let rootViewController = UINavigationController(rootViewController: sampleController)
 
             rootViewController.view.tintColor = UIColor(fromHex: "5182AF")
             rootViewController.navigationBarHidden = true
 
+            FORMDefaultStyle.applyStyle()
+
+            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
             self.window?.rootViewController = rootViewController
             self.window?.makeKeyAndVisible()
         }
