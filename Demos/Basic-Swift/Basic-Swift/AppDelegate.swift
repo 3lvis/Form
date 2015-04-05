c6135b87e88edb8f15cd24c09cc9523bd663d6e3
Christoffer Winterkvist <chris@zenangst.com>
2015-04-05 17:48:44 +0200

    Use 2 space indentation

--- Demos/Basic-Swift/Basic-Swift/AppDelegate.swift
+++ Demos/Basic-Swift/Basic-Swift/AppDelegate.swift
@@ -3,32 +3,32 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        if let JSON: AnyObject? = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json") {
-            let initialValues: Dictionary<NSObject, AnyObject> = [
-                "address"    : "Burger Park 667",
-                "end_date"   : "2017-10-31 23:00:00 +00:00",
-                "first_name" : "Ola",
-                "last_name"  : "Nordman",
-                "start_date" : "2014-10-31 23:00:00 +00:00"]
-            let sampleController = SampleCollectionViewController(initialValues: initialValues, JSON: JSON)
-            let rootViewController = UINavigationController(rootViewController: sampleController)
+    if let JSON: AnyObject? = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json") {
+      let initialValues: Dictionary<NSObject, AnyObject> = [
+        "address"    : "Burger Park 667",
+        "end_date"   : "2017-10-31 23:00:00 +00:00",
+        "first_name" : "Ola",
+        "last_name"  : "Nordman",
+        "start_date" : "2014-10-31 23:00:00 +00:00"]
+      let sampleController = SampleCollectionViewController(initialValues: initialValues, JSON: JSON)
+      let rootViewController = UINavigationController(rootViewController: sampleController)
 
-            rootViewController.view.tintColor = UIColor(fromHex: "5182AF")
-            rootViewController.navigationBarHidden = true
+      rootViewController.view.tintColor = UIColor(fromHex: "5182AF")
+      rootViewController.navigationBarHidden = true
 
-            FORMDefaultStyle.applyStyle()
+      FORMDefaultStyle.applyStyle()
 
-            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-            self.window?.rootViewController = rootViewController
-            self.window?.makeKeyAndVisible()
-        }
-
-        return true
+      self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+      self.window?.rootViewController = rootViewController
+      self.window?.makeKeyAndVisible()
     }
+
+    return true
+  }
 
 }
 
