083bf8e91f2a714c606cf68a1f699c2ca833474f
Christoffer Winterkvist <chris@zenangst.com>
2015-04-07 09:09:32 +0200

    Improve docs

--- Source/FORMDataSource.h
+++ Source/FORMDataSource.h
@@ -224,7 +224,7 @@ includingHiddenFields:(BOOL)includingHiddenFields
 - (void)reloadFieldsAtIndexPaths:(NSArray *)indexPaths;
 
 /*!
- * @param section A section ID for the current section
+ * @param section A section index for the current section
  * @param collectionView The used collectionView, usually from a @c UICollectionViewController.
  */
 - (void)collapseFieldsInSection:(NSInteger)section
