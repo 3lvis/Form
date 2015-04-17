3477316f1f9a9bc063a78dd9e340c8b6253456cb
Elvis Nuñez <elvisnunez@me.com>
2015-04-17 13:06:48 +0200

    Clean up AppDelegate

--- Demos/Login-demo/Login-demo/AppDelegate.m
+++ Demos/Login-demo/Login-demo/AppDelegate.m
@@ -1,14 +1,9 @@
 #import "AppDelegate.h"
 #import "FORMDefaultStyle.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [FORMDefaultStyle applyStyle];
 
     return YES;
