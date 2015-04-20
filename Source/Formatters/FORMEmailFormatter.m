227dfb3e2799509315f920206ef4ef34626ecc8a
Vadym Markov <impressionwave@gmail.com>
2015-04-20 15:30:52 +0200

    Add if braces

--- Source/Formatters/FORMEmailFormatter.m
+++ Source/Formatters/FORMEmailFormatter.m
@@ -4,7 +4,9 @@
 
 - (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse {
     string = [super formatString:string reverse:reverse];
-    if (!string) return nil;
+    if (!string) {
+        return nil;
+    }
 
     return [string stringByReplacingOccurrencesOfString:@"," withString:@""];
 }
