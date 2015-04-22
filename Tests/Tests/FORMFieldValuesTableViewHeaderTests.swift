075520ab93d2d021d42b38c2ff87bebca194bbc0
Elvis Nuñez <elvisnunez@me.com>
2015-04-22 14:01:14 +0200

    Simplify tests

--- Tests/Tests/FORMFieldValuesTableViewHeaderTests.swift
+++ Tests/Tests/FORMFieldValuesTableViewHeaderTests.swift
@@ -4,27 +4,18 @@ import XCTest
 class FORMFieldValuesTableViewHeaderTests: XCTestCase {
 
     func testLabelHeight() {
-        let formField = FORMField()
+        let field = FORMField()
+
         let headerView = FORMFieldValuesTableViewHeader()
+        XCTAssertEqualWithAccuracy(headerView.labelHeight(), CGFloat(68.5), 0.5, "")
 
-        formField.title = "Test"
-        formField.info = "Multi-line\nMulti-line"
+        field.title = "Test"
+        field.info = "Multi-line\nMulti-line"
+        headerView.field = field
+        XCTAssertEqualWithAccuracy(headerView.labelHeight(), CGFloat(77.5), 0.5, "")
 
-        let a = headerView.labelHeight()
-        let b = CGFloat (68.5)
-        XCTAssertEqual(a, b)
-
-        headerView.field = formField
-
-        let c = headerView.labelHeight()
-        let d = CGFloat (78.0)
-        XCTAssertEqual(c, d)
-
-        formField.info = "Multi-line\nMulti-line\nMulti-line"
-        headerView.field = formField
-
-        let f = headerView.labelHeight()
-        let g = CGFloat (98.0)
-        XCTAssertEqual(f, g)
+        field.info = "Multi-line\nMulti-line\nMulti-line"
+        headerView.field = field
+        XCTAssertEqualWithAccuracy(headerView.labelHeight(), CGFloat(97.5), 0.5, "")
     }
 }
