b0666bc078cbe9c05416e70259ef58e71a289e38
Elvis Nuñez <3lvis@users.noreply.github.com>
2015-04-22 10:38:21 +0200

    Update button title [skip ci]

--- Demos/Basic-ObjC/Basic-ObjC/Controllers/HYPSampleCollectionViewController.m
+++ Demos/Basic-ObjC/Basic-ObjC/Controllers/HYPSampleCollectionViewController.m
@@ -125,7 +125,7 @@
 
     self.collectionView.dataSource = self.dataSource;
 
-    UIBarButtonItem *printValuesButton = [[UIBarButtonItem alloc] initWithTitle:@"Values"
+    UIBarButtonItem *printValuesButton = [[UIBarButtonItem alloc] initWithTitle:@"Show Values"
                                                                           style:UIBarButtonItemStyleDone
                                                                          target:self
                                                                          action:@selector(printValuesAction)];
