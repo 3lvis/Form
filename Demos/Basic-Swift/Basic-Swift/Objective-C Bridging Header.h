794fe891baeff68c0ecb4438002b54d371777c89
Elvis Nuñez <elvisnunez@me.com>
2015-05-05 14:35:51 +0200

    Revert "Revert 4407e8c..554132f"
    This reverts commit 036e7a7400ba49c56f4a7b863e47f644236beeec.

--- Demos/Basic-Swift/Basic-Swift/Objective-C Bridging Header.h
+++ Demos/Basic-Swift/Basic-Swift/Objective-C Bridging Header.h
@@ -1,18 +1,7 @@
-#ifndef Basic_Swift_Objective_C_Bridging_Header_h
-#define Basic_Swift_Objective_C_Bridging_Header_h
+@import HYPImagePicker;
 
-#import "FORMDataSource.h"
-#import "FORMPostalCodeManager.h"
-#import "FORMFieldValue.h"
-#import "HYPImagePicker.h"
-#import "FORMData.h"
-#import "FORMTextFieldCell.h"
-#import "FORMDefaultStyle.h"
-#import "FORMViewController.h"
-
-#import "NSObject+HYPTesting.h"
-#import "UIColor+Hex.h"
-#import "NSJSONSerialization+ANDYJSONFile.h"
-#import "UIViewController+HYPKeyboardToolbar.h"
-
-#endif
+@import NSObject_HYPTesting;
+@import Hex;
+@import NSJSONSerialization_ANDYJSONFile;
+@import UIViewController_HYPKeyboardToolbar;
+@import HYPMathParser;
