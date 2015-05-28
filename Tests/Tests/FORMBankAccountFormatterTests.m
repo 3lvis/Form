6b848ac48e39ce90110491cd22a5d660346b3252
Elvis Nuñez <elvisnunez@me.com>
2015-05-28 15:26:47 +0200

    Test FORMBankAccountNumberFormatter

--- Tests/Tests/FORMBankAccountFormatterTests.m
+++ Tests/Tests/FORMBankAccountFormatterTests.m
@@ -1,9 +1,16 @@
 @import XCTest;
 
+#import "FORMBankAccountNumberFormatter.h"
+
 @interface FORMBankAccountFormatterTests : XCTestCase
 
 @end
 
 @implementation FORMBankAccountFormatterTests
+
+- (void)testFormatString {
+    FORMBankAccountNumberFormatter *formatter = [FORMBankAccountNumberFormatter new];
+    XCTAssertEqualObjects([formatter formatString:@"00000000000" reverse:NO], @"0000.00.00000");
+}
 
 @end
