e86c3696e4c8083e80b06d8d4b3588c5f26fb760
Elvis Nuñez <elvisnunez@me.com>
2015-05-28 14:57:22 +0200

    Test testDirectoryFunctionsWithError

--- Tests/Tests/DDMathEvaluator-FORMTests.m
+++ Tests/Tests/DDMathEvaluator-FORMTests.m
@@ -1,9 +1,18 @@
 @import XCTest;
 
+#import "DDMathEvaluator+FORM.h"
+
 @interface DDMathEvaluator_FORMTests : XCTestCase
 
 @end
 
 @implementation DDMathEvaluator_FORMTests
+
+- (void)testDirectoryFunctionsWithError {
+    NSError *error = nil;
+    NSDictionary *dictionary = [DDMathEvaluator hyp_directoryFunctionsWithError:&error];
+    NSArray *keys =  @[@"present", @"equals", @"missing"];
+    XCTAssertEqualObjects(dictionary.allKeys, keys);
+}
 
 @end
