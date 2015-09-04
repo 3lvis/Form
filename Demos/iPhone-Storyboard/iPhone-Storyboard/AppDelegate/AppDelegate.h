ff167f65a24f9966b6104c5ceacf9129011b5876
richardoti <richard@hyper.no>
2015-09-05 01:23:30 +0200

    Refactor AppDelegate: code style

--- Demos/iPhone-Storyboard/iPhone-Storyboard/AppDelegate/AppDelegate.h
+++ Demos/iPhone-Storyboard/iPhone-Storyboard/AppDelegate/AppDelegate.h
@@ -4,6 +4,4 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
--- Demos/iPhone-Storyboard/iPhone-Storyboard/AppDelegate/AppDelegate.m
+++ Demos/iPhone-Storyboard/iPhone-Storyboard/AppDelegate/AppDelegate.m
@@ -1,18 +1,13 @@
 #import "AppDelegate.h"
 #import "FORMDefaultStyle.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
 
   [FORMDefaultStyle applyStyle];
 
   return YES;
 }
+
 @end
