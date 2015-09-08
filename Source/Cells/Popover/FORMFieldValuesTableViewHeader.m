b0ff67151fb4ca44235ce4ad160773fa391f5973
Christoffer Winterkvist <chris@zenangst.com>
2015-09-08 12:18:30 +0200

    Use title instead of rawValue
    This was causing issues when running it with a new version of Xcode.
    
    ```
    [__NSCFNumber isEqualToString:]: unrecognized selector sent to instance
    0x7bb73b10
    ```
    
    The initial value that was assigned to `.text` was a NSNumber and when
    it tried to assign it to a string it couldn’t compare the two.

--- Source/Cells/Popover/FORMFieldValuesTableViewHeader.m
+++ Source/Cells/Popover/FORMFieldValuesTableViewHeader.m
@@ -36,7 +36,7 @@
     _titleLabel.textAlignment = NSTextAlignmentCenter;
     _titleLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
     _titleLabel.numberOfLines = 0;
-    _titleLabel.text = self.field.rawFieldValue;
+    _titleLabel.text = self.field.title;
 
     return _titleLabel;
 }
