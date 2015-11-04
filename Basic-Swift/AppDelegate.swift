9d89f4fb2aca305d8c0da39c43f52b760a830ee8
Elvis Nuñez <elvisnunez@me.com>
2015-11-04 16:02:40 +0100

    Use sane formatting

--- Basic-Swift/AppDelegate.swift
+++ Basic-Swift/AppDelegate.swift
@@ -6,32 +6,32 @@ import Hex
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
+    var window: UIWindow?
 
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-    if let JSON = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json") as? [String : AnyObject] {
-      let initialValues = [
-        "address"    : "Burger Park 667",
-        "end_date"   : "2017-10-31 23:00:00 +00:00",
-        "first_name" : "Ola",
-        "last_name"  : "Nordman",
-        "start_date" : "2014-10-31 23:00:00 +00:00"]
-      let sampleController = SampleCollectionViewController(JSON: JSON, initialValues: initialValues)
-      let rootViewController = UINavigationController(rootViewController: sampleController)
+        if let JSON = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json") as? [String : AnyObject] {
+            let initialValues = [
+                "address"    : "Burger Park 667",
+                "end_date"   : "2017-10-31 23:00:00 +00:00",
+                "first_name" : "Ola",
+                "last_name"  : "Nordman",
+                "start_date" : "2014-10-31 23:00:00 +00:00"]
+            let sampleController = SampleCollectionViewController(JSON: JSON, initialValues: initialValues)
+            let rootViewController = UINavigationController(rootViewController: sampleController)
 
-      rootViewController.view.tintColor = UIColor(hex: "5182AF")
-      rootViewController.navigationBarHidden = true
+            rootViewController.view.tintColor = UIColor(hex: "5182AF")
+            rootViewController.navigationBarHidden = true
 
-      FORMDefaultStyle.applyStyle()
+            FORMDefaultStyle.applyStyle()
 
-      self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-      self.window?.rootViewController = rootViewController
-      self.window?.makeKeyAndVisible()
+            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+            self.window?.rootViewController = rootViewController
+            self.window?.makeKeyAndVisible()
+        }
+        
+        return true
     }
-
-    return true
-  }
-
+    
 }
 
