7ee9a390c1f3f4da906bf621f6bf8eff7050e460
Elvis Nuñez <elvisnunez@me.com>
2015-11-04 15:58:01 +0100

    Add Basic-Swift

--- Basic-Swift/AppDelegate.swift
+++ Basic-Swift/AppDelegate.swift
@@ -1,49 +1,37 @@
-//
-//  AppDelegate.swift
-//  Basic-Swift
-//
-//  Created by Elvis Nuñez on 04/11/15.
-//
-//
-
 import UIKit
+import Form.FORMDefaultStyle
+import NSJSONSerialization_ANDYJSONFile
+import Hex
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        // Override point for customization after application launch.
-        self.window!.backgroundColor = UIColor.whiteColor()
-        self.window!.makeKeyAndVisible()
-        return true
+    if let JSON = NSJSONSerialization.JSONObjectWithContentsOfFile("forms.json") as? [String : AnyObject] {
+      let initialValues = [
+        "address"    : "Burger Park 667",
+        "end_date"   : "2017-10-31 23:00:00 +00:00",
+        "first_name" : "Ola",
+        "last_name"  : "Nordman",
+        "start_date" : "2014-10-31 23:00:00 +00:00"]
+      let sampleController = SampleCollectionViewController(JSON: JSON, initialValues: initialValues)
+      let rootViewController = UINavigationController(rootViewController: sampleController)
+
+      rootViewController.view.tintColor = UIColor(hex: "5182AF")
+      rootViewController.navigationBarHidden = true
+
+      FORMDefaultStyle.applyStyle()
+
+      self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+      self.window?.rootViewController = rootViewController
+      self.window?.makeKeyAndVisible()
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
+    return true
+  }
 
 }
 
