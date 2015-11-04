0b98f15c3f20fc552001c469074679c0f73e4fe1
Vadym Markov <markov.vadym@gmail.com>
2015-11-04 15:15:38 +0100

    Use 4 indent

--- Tests/Tests/FORMInputValidatorTests.m
+++ Tests/Tests/FORMInputValidatorTests.m
@@ -1,5 +1,4 @@
 @import XCTest;
-@import Foundation;
 
 #import "FORMInputValidator.h"
 
@@ -10,42 +9,42 @@
 @implementation FORMInputValidatorTests
 
 - (void)testValidateReplacementStringWithInvalidRange {
-  FORMInputValidator *inputValidator = [[FORMInputValidator alloc] init];
-  FORMFieldValidation *fieldValidation = [[FORMFieldValidation alloc]
-                                          initWithDictionary:@{@"required": @YES,
-                                                               @"max_value": @19.9}];
-  inputValidator.validation = fieldValidation;
-  BOOL isValid = [inputValidator validateReplacementString:@"1"
-                                                  withText:@"10.0"
-                                                 withRange:NSMakeRange(5, 1)];
+    FORMInputValidator *inputValidator = [[FORMInputValidator alloc] init];
+    FORMFieldValidation *fieldValidation = [[FORMFieldValidation alloc]
+                                            initWithDictionary:@{@"required": @YES,
+                                                                 @"max_value": @19.9}];
+    inputValidator.validation = fieldValidation;
+    BOOL isValid = [inputValidator validateReplacementString:@"1"
+                                                    withText:@"10.0"
+                                                   withRange:NSMakeRange(5, 1)];
 
-  XCTAssertFalse(isValid);
+    XCTAssertFalse(isValid);
 }
 
 - (void)testValidateReplacementStringWithValidRange {
-  FORMInputValidator *inputValidator = [[FORMInputValidator alloc] init];
-  FORMFieldValidation *fieldValidation = [[FORMFieldValidation alloc]
-                                          initWithDictionary:@{@"required": @YES,
-                                                               @"max_value": @19.9}];
-  inputValidator.validation = fieldValidation;
-  BOOL isValid = [inputValidator validateReplacementString:@"1"
-                                                  withText:@"10.0"
-                                                 withRange:NSMakeRange(4, 1)];
+    FORMInputValidator *inputValidator = [[FORMInputValidator alloc] init];
+    FORMFieldValidation *fieldValidation = [[FORMFieldValidation alloc]
+                                            initWithDictionary:@{@"required": @YES,
+                                                                 @"max_value": @19.9}];
+    inputValidator.validation = fieldValidation;
+    BOOL isValid = [inputValidator validateReplacementString:@"1"
+                                                    withText:@"10.0"
+                                                   withRange:NSMakeRange(4, 1)];
 
-  XCTAssertTrue(isValid);
+    XCTAssertTrue(isValid);
 }
 
 - (void)testValidateReplacementStringWithInvalidString {
-  FORMInputValidator *inputValidator = [[FORMInputValidator alloc] init];
-  FORMFieldValidation *fieldValidation = [[FORMFieldValidation alloc]
-                                          initWithDictionary:@{@"required": @YES,
-                                                               @"max_value": @19.9}];
-  inputValidator.validation = fieldValidation;
-  BOOL isValid = [inputValidator validateReplacementString:@"1000"
-                                                  withText:@"10.0"
-                                                 withRange:NSMakeRange(2, 1)];
-
-  XCTAssertFalse(isValid);
+    FORMInputValidator *inputValidator = [[FORMInputValidator alloc] init];
+    FORMFieldValidation *fieldValidation = [[FORMFieldValidation alloc]
+                                            initWithDictionary:@{@"required": @YES,
+                                                                 @"max_value": @19.9}];
+    inputValidator.validation = fieldValidation;
+    BOOL isValid = [inputValidator validateReplacementString:@"1000"
+                                                    withText:@"10.0"
+                                                   withRange:NSMakeRange(2, 1)];
+    
+    XCTAssertFalse(isValid);
 }
 
 @end
