4dc57751f7f8e0dc3e61643537be4fe60a183a23
Elvis Nuñez <elvisnunez@me.com>
2015-11-19 10:34:39 +0100

    Add modal demo

--- ModalForm/AppDelegate.swift
+++ ModalForm/AppDelegate.swift
@@ -1,11 +1,15 @@
 import UIKit
 
+import Form
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        FORMDefaultStyle.applyStyle()
+
         return true
     }
 }
