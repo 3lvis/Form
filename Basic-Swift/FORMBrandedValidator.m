3a157de48b25f22179b17bca4c7c9cce26a00a9c
Elvis Nuñez <elvisnunez@me.com>
2017-03-07 23:28:30 +0100

    Use super

--- Basic-Swift/FORMBrandedValidator.m
+++ Basic-Swift/FORMBrandedValidator.m
@@ -3,6 +3,9 @@
 @implementation FORMBrandedValidator
 
 - (FORMValidationResultType)validateFieldValue:(id)fieldValue {
+    FORMValidationResultType superValidation = [super validateFieldValue:fieldValue];
+    if (superValidation != FORMValidationResultTypeValid) return superValidation;
+
     return FORMValidationResultTypeInvalid;
 }
 
