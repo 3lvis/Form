03a33c0b50383c39411581125db8839118cf30bd
Elvis Nuñez <elvisnunez@me.com>
2017-07-20 13:16:25 +0200

    Enable iPad support for modal demo

--- ModalForm/ViewController.swift
+++ ModalForm/ViewController.swift
@@ -4,6 +4,11 @@ import Form
 class ViewController: UIViewController {
     @IBAction func showForm(_: UIButton) {
         let formController = SampleFormViewController()
+
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            formController.modalPresentationStyle = .formSheet
+        }
+
         self.present(formController, animated: true, completion: nil)
     }
 }
